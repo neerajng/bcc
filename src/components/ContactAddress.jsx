38e7a8bb4780474907f2abe0d9d956a74ca8b67f
neerajng <neerajnrjng@gmail.com>
2024-04-06 12:59:56 +0530

    Fixed cantact us UI.

--- src/components/ContactAddress.jsx
+++ src/components/ContactAddress.jsx
@@ -1,17 +1,17 @@
-const ContactForm = () => {
+const ContactAddress = () => {
     return (
-        <div className="bg-white p-8 rounded-lg flex flex-col shadow-lg">
-             <div className="border-b border-gray-900/10 pb-12 ">
-             <h2 className="text-base leading-7  text-indigo-600 font-bold">Conatct US</h2>
-              <p className="text-2xl font-bold">Let&apos;s collaborate.</p>  
-              <p>Park Centre, Technopark Campus, Kazhakkuttam, <br/>
-                TVM, Kerala, India 94158 <br/>
-                info@bcc.com<br/>
-                Tel: 123-456-7890<br/>
+        <div className="bg-white p-8 rounded-lg flex flex-col shadow-lg place-content-center lg:my-36">
+             <div className="border-b  border-gray-900/10 pb-12">
+                <h2 className="text-base text-indigo-600 leading-7 font-bold">Contact Us</h2>
+                <p className="text-2xl font-bold">Let&apos;s collaborate.</p>  
+                <p>Park Centre, Technopark Campus, Kazhakkuttam, <br/>
+                    TVM, Kerala, India 94158 <br/>
+                    info@bcc.com<br/>
+                    Tel: 123-456-7890<br/>
                 </p>
              </div>
         </div>
     );
 };
 
-export default ContactForm;
+export default ContactAddress;
